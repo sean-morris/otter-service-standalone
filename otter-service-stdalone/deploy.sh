d9baa70ce334f3b5fc0772c00d706f3c436a2c0e
sean-morris <sean.smorris@gmail.com>
2023-02-09 17:48:11 -0800

    got verbose logging

--- otter-service-stdalone/deploy.sh
+++
@@ -1,5 +0,0 @@
-branch_name=$(git symbolic-ref -q HEAD)
-branch_name=${branch_name##refs/heads/}
-branch_name=${branch_name:-HEAD}
-helm upgrade --install otter-srv otter-service-stdalone --values otter-service-stdalone/values.yaml --values otter-service-stdalone/values.dev.yaml --namespace otter-stdalone-$branch_name
-
