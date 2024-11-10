20b3469dbe94e94c6c3cd59436390649bbbf1e62
sean-morris <sean.smorris@gmail.com>
2024-11-10 13:53:44 -0800

    Captures Otter Version 5 or Greater

--- src/otter_service_stdalone/util.py
+++ src/otter_service_stdalone/util.py
@@ -2,7 +2,7 @@ import re
 import zipfile
 
 
-def is_version_5(zip_ref, target_file, reg):
+def is_version_5_or_greater(zip_ref, target_file, reg):
     if target_file:
         with zip_ref.open(target_file) as file:
             content = file.read().decode('utf-8')
@@ -16,8 +16,8 @@ def is_version_5(zip_ref, target_file, reg):
 
 
 def otter_version_correct(autograder_path):
-    requirements_regex = re.compile(r"otter-grader==([\d\.]+)")
-    environment_regex = re.compile(r"otter-grader==([\d\.]+)")
+    requirements_regex = re.compile(r"(otter-grader)(\[[\w,]+\])?==([\d\.]+)")
+    environment_regex = re.compile(r"(otter-grader)(\[[\w,]+\])?==([\d\.]+)")
     # Open the zip file
     with zipfile.ZipFile(autograder_path, 'r') as zip_ref:
         # Get a list of files in the zip
@@ -31,6 +31,6 @@ def otter_version_correct(autograder_path):
         if 'environment.yml' in file_list:
             env_target_file = 'environment.yml'
 
-        otter_in_req = is_version_5(zip_ref, req_target_file, requirements_regex)
-        otter_in_env = is_version_5(zip_ref, env_target_file, environment_regex)
+        otter_in_req = is_version_5_or_greater(zip_ref, req_target_file, requirements_regex)
+        otter_in_env = is_version_5_or_greater(zip_ref, env_target_file, environment_regex)
         return otter_in_req or otter_in_env
