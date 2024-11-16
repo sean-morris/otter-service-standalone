2e4ae91886007c8de4b26fc52f12d9a2caa69e4c
sean-morris <sean.smorris@gmail.com>
2024-11-16 07:35:42 -0800

    Otter-Grader Version warning

--- src/otter_service_stdalone/app.py
+++ src/otter_service_stdalone/app.py
@@ -277,12 +277,13 @@ class Upload(BaseHandler):
             m = "Step 3A: Uploaded autograder.zip files written to disk - now checking otter version"
             log.write_logs(results_path, m, f"Results Code: {results_path}", "debug", log_debug)
             if not otter_version_correct(auto_p):
-                m = "Step 3A-1: autograder.zip is wrong version of otter-grader; must be >= 5.5.0"
+                m = "Step 3A-1: autograder.zip is wrong version of otter-grader; must be >= 6.0.4"
                 log.write_logs(results_path, m, f"Results Code: {results_path}", "debug", log_debug)
-                m = "You need to make sure the autograder.zip uses otter-grader version >=5.5.0. BUT you do not need "
-                m += "to re-generate your autograder.zip; un-archive autograder.zip and change the version in either "
-                m += "requirements.txt or environmental.yaml depending on the version of otter-grader the autograder.zip "
-                m += "is created with."
+                m = "If you are teaching Data 8, please get the latest autograder.zips from materials-sp22-private. "
+                m += "If you are NOT teaching Berkeley's Data 8, you need to make sure the autograder.zip uses "
+                m += "otter-grader version >=6.0.4; due to breaking changes in "
+                m += "otter-grader dependencies you need to run otter assign with otter-grader >= 6.0.4 to generate working "
+                m += "solution files(autograder.zips)."
                 self.render("index.html", message=m)
             else:
                 fh = open(notebooks_path, 'wb')
